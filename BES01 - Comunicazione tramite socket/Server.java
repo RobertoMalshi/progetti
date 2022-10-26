99320c7e5bc45d104fdfa1ac99f7c88f5d07d57d
RobertoMalshi <115538636+RobertoMalshi@users.noreply.github.com>
2022-10-26 15:57:28 +0200

    Update and rename Server.java to BES01 - Comunicazione tramite socket/Server.java

--- BES01 - Comunicazione tramite socket/Server.java
+++ BES01 - Comunicazione tramite socket/Server.java
@@ -13,24 +13,24 @@ public class Server {
 			 
 			int severPort=8698;
 			String clientMessage = "";
-			
+
 			// Listen to port
 			ServerSocket server = new ServerSocket(severPort);
 			System.out.println("Server: in ascolto sula porta " + severPort );
 
 			Socket serverClientSocket = server.accept();
-			
+
 			DataInputStream inStream = new DataInputStream(serverClientSocket.getInputStream());
 			DataOutputStream outStream = new DataOutputStream(serverClientSocket.getOutputStream());
-			
-			
-			
+
 			while(!clientMessage.equals("end")) {
-		      clientMessage = inStream.readUTF();
-		      System.out.println("Server: ricevuto messaggio " + clientMessage );
-		      System.out.println("Server: invio messaggio "    + clientMessage );
-			  outStream.writeUTF("Echo from server : "         + clientMessage);
-		      outStream.flush();
+				
+				clientMessage = inStream.readUTF();
+				System.out.println("Server: ricevuto messaggio " + clientMessage );
+				
+				System.out.println("Server: invio messaggio "    + clientMessage );
+				outStream.writeUTF("Echo from server : "         + clientMessage);
+				outStream.flush();
 			}
 		} catch (Exception e) {
 			System.out.println(e);
